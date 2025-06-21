c3f0f72dc94e5b9c9682c424e225629596727a9d
joy-mahmud <joymahmudiucse@gmail.com>
2025-06-21 23:07:01 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h2>Hello react</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+import AllFoods from "../Pages/AllFoods/AllFoods";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: 'allfoods',
+                element: <AllFoods />
+            }
+        ]
+    },
+]);
+
+export default router
