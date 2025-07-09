7e4289d053f35da6ca12caa143a5c952b9075695
joy-mahmud <joymahmudiucse@gmail.com>
2025-07-09 22:53:33 +0600

    get the food items from the backend

--- src/Pages/AllFoods/AllFoods.jsx
+++ src/Pages/AllFoods/AllFoods.jsx
@@ -1,46 +1,23 @@
-import React from 'react'
-import food1 from '../../assets/images/food1.jpg'
-import food2 from '../../assets/images/food2.jpg'
-import food3 from '../../assets/images/food3.jpg'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../../utils/constants'
 const AllFoods = () => {
-    const data = [{
-        "title": "Margherita Pizza",
-        "description": "Classic pizza with tomato sauce, mozzarella, and basil.",
-        "price": 8.99,
-        "quantity": 10,
-        "origin": "Italy",
-        "category": "Fast Food",
-        "rating": 4.5,
-        "image": food1
-    },
-    {
-        "title": "Chicken Biryani",
-        "description": "Fragrant rice cooked with spiced chicken and saffron.",
-        "price": 10.5,
-        "quantity": 5,
-        "origin": "India",
-        "category": "Main Course",
-        "rating": 4.8,
-        "image": food2
-    },
-    {
-        "title": "Caesar Salad",
-        "description": "Fresh romaine lettuce with creamy Caesar dressing.",
-        "price": 6.75,
-        "quantity": 0,
-        "origin": "Mexico",
-        "category": "Salad",
-        "rating": 4.2,
-        "image": food3
-    }
+    const [foods, setFoods] = useState(null)
 
-    ]
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get(`${BASE_URL}/api/all-foods/`)
+            const data = res.data
+            setFoods(data)
+        }
+        fetchData()
+    }, [])
     return (
         <div>
             <h2 className='text-2xl my-5 font-semibold text-center'>All Foods</h2>
             <div className='grid grid-cols-3 gap-5 p-5'>
                 {
-                    data.map((item, idx) => (
+                    foods ? foods.map((item, idx) => (
                         <div key={idx} className='bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 hover:cursor-pointer'>
                             <img src={item.image} alt="img" className='w-full h-48 rounded-t-xl' />
                             <div className='p-5'>
@@ -55,7 +32,14 @@ const AllFoods = () => {
                             </div>
 
                         </div>
-                    ))
+                    )) : Array.from({ length: 6 }, (_, index) => <div key={index} className="flex w-52 flex-col gap-4">
+                        <div className="skeleton h-32 w-full"></div>
+                        <div className="skeleton h-4 w-28"></div>
+                        <div className="skeleton h-4 w-full"></div>
+                        <div className="skeleton h-4 w-full"></div>
+                    </div>)
+
+
                 }
             </div>
         </div>
