0e73b551f54d89d210b2b3e08a7022827e22f17b
joy-mahmud <joymahmudiucse@gmail.com>
2025-09-27 23:25:58 +0600

    recent orders and recent users section in dashboard home

--- src/Pages/Dashboard/AllOrders.jsx
+++ src/Pages/Dashboard/AllOrders.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { BASE_URL } from '../../utils/constants'
 import TableSkeleten from '../../components/common/TableSkeleten'
 import OrderItemsModal from '../../components/OrderItemModal/OrderItemsModal'
+import { formatDateTime } from '../../utils/formatDateTime'
 const AllOrders = () => {
     const [allOrders, setAllOrders] = useState(null)
     const [page, setPage] = useState(1)
@@ -39,30 +40,13 @@ const AllOrders = () => {
             setPage(prev => Number(prev) + 1)
         }
     }
-    function formatDateTime(datetimeStr) {
-        try {
-            const date = new Date(datetimeStr);
-            const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, "0");
-            const day = String(date.getDate()).padStart(2, "0");
 
-            let hours = date.getHours();
-            const minutes = String(date.getMinutes()).padStart(2, "0");
-
-            const ampm = hours >= 12 ? "PM" : "AM";
-            hours = hours % 12 || 12;
-
-            return `${year}-${month}-${day} ${hours}:${minutes} ${ampm}`;
-        } catch (err) {
-            throw new Error("Invalid datetime string format", err);
-        }
-    }
 
 
     return (
         <div>
 
-            <h2 className='text-center font-bold text-2xl py-5'>All Foods</h2>
+            <h2 className='text-center font-bold text-2xl py-5'>All Orders</h2>
             <div className="overflow-x-auto rounded-box border-[2px] border-base-content/5 bg-base-100">
                 <table className="table">
                     {/* head */}
--- src/Pages/Dashboard/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome.jsx
@@ -4,7 +4,9 @@ import { BASE_URL } from '../../utils/constants'
 import DashboardHero from '../../components/dashboard/DashboardHero'
 import MetricsCard from '../../components/dashboard/MetricsCard'
 import { LuCircleDollarSign, LuShoppingBag, LuUsers, LuUtensilsCrossed } from 'react-icons/lu'
-import { FaRegStar } from 'react-icons/fa'
+import { FaArrowRight, FaRegStar } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { formatDateTime } from '../../utils/formatDateTime'
 
 const DashboardHome = () => {
     const [stats, setStats] = useState(null)
@@ -23,8 +25,9 @@ const DashboardHome = () => {
         return
     }
     console.log(Object.keys(stats))
+    const label_bg = { "FAILED": "#D12A24", "CONFIRMED": "#24D129", "PENDING": "#CED124" }
     return (
-        <div className='bg-gray-100 h-full'>
+        <div className='bg-gray-100 mb-10'>
             <DashboardHero />
 
 
@@ -57,25 +60,96 @@ const DashboardHome = () => {
                     icon={<LuShoppingBag className='text-orange-600' size={22} />}
                 />
             </div>
+            <div className='grid grid-cols-2 gap-5 px-5'>
+                <div className='border-2 border-gray-200 bg-white p-5 rounded-xl flex flex-col w-full'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='text-2xl font-bold mb-2'>Recent Users</h2>
+                        <span className='text-green-500'>
+                            <Link to={'/dashboard/all-users'} className='flex gap-2 items-center'>
+                                <span>View all</span>
+                                <FaArrowRight />
+                            </Link>
 
-            {/* <div className='flex gap-5'>
-                <div className='h-20 flex justify-center items-center rounded-lg border w-[190px]'>
-                    <div>Toatal revenue:{stats?.total_revenue}</div>
-                </div>
-                <div className='h-20 flex justify-center items-center rounded-lg border w-[190px]'>
-                    <div>Total foods: {stats?.food_count}</div>
-                </div>
-                <div className='h-20 flex justify-center items-center rounded-lg border w-[190px]'>
-                    <div>Average rating:{stats?.avg_rating}</div>
-                </div>
-                <div className='h-20 flex justify-center items-center rounded-lg border w-[190px]'>
-                    <div>Total users:{stats.user_count}</div>
-                </div>
-                <div className='h-20 flex justify-center items-center rounded-lg border w-[190px]'>
-                    <div>Total Orders:{stats?.total_orders}</div>
-                </div>
-            </div> */}
+                        </span>
+                    </div>
+                    <div className='space-y-2'>
 
+                        {
+                            stats && stats?.recent_users.map((user, index) => (
+                                <div key={index} className='bg-gray-200 rounded-lg px-3 py-1 space-y-2'>
+                                    <div className='flex justify-between items-start'>
+                                        <div>
+                                            <h3 className='text-[16px] font-semibold'>{user.name}</h3>
+                                            <p className='text-gray-500 text-xs'>{user.email}</p>
+                                        </div>
+                                        <p className='text-[16px] font-medium'>{formatDateTime(user.created_at)}</p>
+                                    </div>
+
+
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                </div>
+                <div className='border-2 border-gray-200 bg-white p-5 rounded-xl flex flex-col w-full'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='text-2xl font-bold mb-2'>Recent Users</h2>
+                        <span className='text-green-500'>
+                            <Link to={'/dashboard/all-orders'} className='flex gap-2 items-center'>
+                                <span>View all</span>
+                                <FaArrowRight />
+                            </Link>
+
+                        </span>
+                    </div>
+                    <div className='space-y-2'>
+                        {
+                            stats && stats?.recent_orders.map((order, index) => (
+                                <div key={index} className='bg-gray-200 rounded-lg px-3 py-1 flex justify-between items-center'>
+                                    <div>
+                                        <h3 className='text-[18px] font-semibold'>{order.user}</h3>
+                                        <p className='text-gray-500 text-xs'>{formatDateTime(order.created_at)}</p>
+                                    </div>
+                                    <div>
+                                        <p className='font-semibold text-[18px]'>${order.total_amount}</p>
+                                        <span
+                                            style={{ backgroundColor: label_bg[order.status] }}
+                                            className={`text-xs font-extralight px-3 py-[2px] rounded-full`}>
+                                            {order.status.toLowerCase()}
+                                        </span>
+                                    </div>
+
+                                </div>
+                            ))
+                        }
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div className='border-2 border-gray-200 bg-white p-5 rounded-xl mx-5 mt-5'>
+                <h2 className='text-xl font-semibold mb-5'>Performance Overview</h2>
+                <div className='flex justify-around'>
+                    <div className='text-center'>
+
+                        <p className='text-green-500 text-2xl font-semibold'>95%</p>
+                        <p className='text-xs text-gray-600'>Customer Satisfaction</p>
+                    </div>
+                    <div className='text-center'>
+
+                        <p className='text-orange-600 text-2xl font-semibold'>20 min</p>
+                        <p className='text-xs text-gray-600'>Avg Delivery Time</p>
+                    </div>
+                    <div className='text-center'>
+
+                        <p className='text-yellow-500 text-2xl font-semibold'>4.2</p>
+                        <p className='text-xs text-gray-600'>Orders Per Day</p>
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     )
